a9573ef4a540d50ffbfae8a39723ea92b468b397
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-01-12 18:24:05 +0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 
 // JQuery
 
-$(document).ready(function(){
-  $('.skill-category-title').click(function(){
-    $('.skills-list').hide();
+$(document).ready(function () {
+  $(".skill-category-title").click(function () {
+    $(".skills-list").hide();
 
     var index = $(this).index();
-    var skillList = $('.skills-list').eq(index);
+    var skillList = $(".skills-list").eq(index);
 
-    if(skillList.is(':visible')){
+    if (skillList.is(":visible")) {
       skillList.hide();
     } else {
-      $('.skills-list').hide();
+      $(".skills-list").hide();
       skillList.show();
     }
   });
 
-  $('.language-skills').show();
+  $(".language-skills").show();
 
-  $('.skill-category-title.language h4').css('color', 'hsl(45, 100%, 72%)');
+  $(".skill-category-title.language h4").css("color", "hsl(45, 100%, 72%)");
 
-  $('.skills-item .title-wrapper').click(function(){
-    var skillTitle = $(this).find('h5').text();
-    
-    console.log(skillTitle); 
+  $(".skills-item .title-wrapper").click(function () {
+    var skillTitle = $(this).find("h5").text();
+
+    console.log(skillTitle);
   });
 });
 
 function changeColor(clickedElement) {
-  var elements = document.querySelectorAll('.skill-category-title h4');
+  var elements = document.querySelectorAll(".skill-category-title h4");
 
-  elements.forEach(function(element) {
+  elements.forEach(function (element) {
     if (element === clickedElement) {
       element.style.color = "hsl(45, 100%, 72%)";
     } else {
@@ -49,8 +49,6 @@ sidebarBtn.addEventListener("click", function () {
   sidebar.classList.toggle("active");
 });
 
-
-
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
@@ -66,29 +64,27 @@ const modalText = document.querySelector("[data-modal-text]");
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
   testimonialsItem[i].addEventListener("click", function () {
-
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalTitle.innerHTML = this.querySelector(
+      "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      "[data-testimonials-text]"
+    ).innerHTML;
 
     testimonialsModalFunc();
-
   });
-
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -96,14 +92,13 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-
 function elementToggleFunc(element) {
   // Lakukan operasi toggle pada elemen yang diberikan (misalnya, tampilkan atau sembunyikan)
-  element.classList.toggle('active');
+  element.classList.toggle("active");
 }
 
 // Menggunakan event listener pada button dengan id "select"
-select.addEventListener('click', function() {
+select.addEventListener("click", function () {
   // Panggil fungsi elementToggleFunc dan kirimkan elemen this (button) sebagai argumen
   elementToggleFunc(this);
 });
@@ -111,12 +106,10 @@ select.addEventListener('click', function() {
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
-
   });
 }
 
@@ -124,9 +117,7 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -134,18 +125,14 @@ const filterFunc = function (selectedValue) {
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
-}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
   filterBtn[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
@@ -153,59 +140,54 @@ for (let i = 0; i < filterBtn.length; i++) {
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
-
   });
-
 }
 
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby-TIGP5hd3qZDQkAWczflI3qVDorrRt3egys1dGRolltGTEX6xI5XdumXT7CQdFqiAvA/exec";
+const form = document.forms["submit-to-google-sheet"];
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = document.forms["submit-to-google-sheet"];
+  const nameInput = form.elements["name"];
+  const emailInput = form.elements["email"];
+  const messageInput = form.elements["pesan"];
 
-const scriptURL = "https://script.google.com/macros/s/AKfycby-TIGP5hd3qZDQkAWczflI3qVDorrRt3egys1dGRolltGTEX6xI5XdumXT7CQdFqiAvA/exec";
-      const form = document.forms["submit-to-google-sheet"];
+  if (!nameInput.value || !emailInput.value || !messageInput.value) {
+    // Menampilkan SweetAlert jika terjadi kesalahan input
+    Swal.fire({
+      icon: "error",
+      title: "Kesalahan",
+      text: "Harap lengkapi semua kolom sebelum mengirim pesan",
+    });
+    return;
+  }
 
-      function handleSubmit(event) {
-        event.preventDefault();
-        const form = document.forms["submit-to-google-sheet"];
-        const nameInput = form.elements["name"];
-        const emailInput = form.elements["email"];
-        const messageInput = form.elements["pesan"];
-
-        if (!nameInput.value || !emailInput.value || !messageInput.value) {
-          // Menampilkan SweetAlert jika terjadi kesalahan input
-          Swal.fire({
-            icon: "error",
-            title: "Kesalahan",
-            text: "Harap lengkapi semua kolom sebelum mengirim pesan",
-          });
-          return;
-        }
-
-        fetch(scriptURL, { method: "POST", body: new FormData(form) })
-          .then((response) => {
-            console.log("Success!", response);
-            // Menampilkan SweetAlert setelah pengiriman berhasil
-            Swal.fire({
-              icon: "success",
-              title: "Berhasil",
-              text: "Pesan telah terkirim",
-              showConfirmButton: false,
-              timer: 2000, // Menutup otomatis pesan setelah 2 detik
-            });
-            // Mereset inputan
-            form.reset();
-          })
-          .catch((error) => {
-            console.error("Error!", error.message);
-            // Menampilkan SweetAlert jika terjadi kesalahan
-            Swal.fire({
-              icon: "error",
-              title: "Kesalahan",
-              text: "Terjadi kesalahan saat mengirim pesan",
-            });
-          });
-      }
-
-
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      // Menampilkan SweetAlert setelah pengiriman berhasil
+      Swal.fire({
+        icon: "success",
+        title: "Berhasil",
+        text: "Pesan telah terkirim",
+        showConfirmButton: false,
+        timer: 2000, // Menutup otomatis pesan setelah 2 detik
+      });
+      // Mereset inputan
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      // Menampilkan SweetAlert jika terjadi kesalahan
+      Swal.fire({
+        icon: "error",
+        title: "Kesalahan",
+        text: "Terjadi kesalahan saat mengirim pesan",
+      });
+    });
+}
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -215,10 +197,10 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
     const targetPage = this.innerHTML.toLowerCase();
-    
+
     for (let j = 0; j < pages.length; j++) {
       const currentPage = pages[j].dataset.page.toLowerCase();
-      
+
       if (targetPage === currentPage) {
         pages[j].classList.add("active");
         navigationLinks[i].classList.add("active");
