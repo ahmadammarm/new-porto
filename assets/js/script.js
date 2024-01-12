f422d35baf3fbfa618878626fb6d2669df150042
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-01-12 12:32:20 +0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -214,17 +214,19 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
+    const targetPage = this.innerHTML.toLowerCase();
+    
+    for (let j = 0; j < pages.length; j++) {
+      const currentPage = pages[j].dataset.page.toLowerCase();
+      
+      if (targetPage === currentPage) {
+        pages[j].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
       }
     }
-
   });
 }
